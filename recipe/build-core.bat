84b48e59eba5a70a3271759897a0fc0731d5c783
mattip <matti.picus@gmail.com>
2023-06-15 23:13:35 +0300

    work around bazel issue

--- recipe/build-core.bat
+++ recipe/build-core.bat
@@ -3,15 +3,20 @@ echo on
 set SKIP_THIRDPARTY_INSTALL=1
 set IS_AUTOMATED_BUILD=1
 set "BAZEL_SH=%BUILD_PREFIX%\Library\usr\bin\bash.exe"
+set CC_CONFIGURE_DEBUG=1
 
+echo ==========================================================
+echo try to work around https://github.com/bazelbuild/bazel/issues/18592
+echo ==========================================================
 set BAZEL_VC=C:\Program Files\Microsoft Visual Studio\2022\Enterprise\VC
-
 echo dir %BAZEL_VC%
-dir %BAZEL_VC%
-echo PATH %PATH%
-echo INCLUDE %INCLUDE%
+rmdir /q /s %BAZEL_VC%\vcpkg
+echo dir %BAZEL_VC%
 
+
+echo ==========================================================
 echo calling pip install
+echo ==========================================================
 
 "%PYTHON%" -m pip install . -vv
 rem remember the return code
